758cebd1471479a99e386e94cee1d85727c0bc80
rajeev-the <146732212+rajeev-the@users.noreply.github.com>
2025-08-12 12:56:26 +0530

    Update Loginuser.jsx

--- src/pages/Loginuser.jsx
+++ src/pages/Loginuser.jsx
@@ -79,10 +79,7 @@ const Loginuser = () => {
         <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p>
       </div>
       <div>
-        <Link
-          to='/captain-login'
-          className='bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
-        >Sign in as Captain</Link>
+        
       </div>
     </div>
   )
